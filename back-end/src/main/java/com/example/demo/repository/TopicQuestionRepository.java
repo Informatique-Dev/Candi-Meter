3e5516b73f6b24c6b88c1bbbc28e169c9cefac60
Noorhn-Khaled <100763814+Noorhan-Khaled@users.noreply.github.com>
2022-03-30 18:43:29 +0200

    delete

--- back-end/src/main/java/com/example/demo/repository/TopicQuestionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.TopicQustion;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TopicQuestionRepository extends CrudRepository<TopicQustion,Long> {
-}
--- back-end/src/main/java/com/example/demo/repository/TopicsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Topics;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TopicsRepository extends CrudRepository<Topics,Long> {
-}
