903c3510ba29486a9da5766b6558078ca598fd41
Noorhn-Khaled <100763814+Noorhan-Khaled@users.noreply.github.com>
2022-04-04 14:47:51 +0200

    map position

---
+++ back-end/src/main/java/com/example/demo/dto/PositionDto.java
@@ -0,0 +1,24 @@
+package com.example.demo.dto;
+
+import javax.persistence.Column;
+
+public class PositionDto {
+    private Long positionId;
+    private String positionName;
+
+    public Long getPositionId() {
+        return positionId;
+    }
+
+    public void setPositionId(Long positionId) {
+        this.positionId = positionId;
+    }
+
+    public String getPositionName() {
+        return positionName;
+    }
+
+    public void setPositionName(String positionName) {
+        this.positionName = positionName;
+    }
+}
