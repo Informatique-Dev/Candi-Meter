d42dbec9d9634bc017fe2af513b0f7d81cd98666
Noorhn-Khaled <100763814+Noorhan-Khaled@users.noreply.github.com>
2022-04-05 23:23:48 +0200

    update mapstruct

--- back-end/src/main/java/com/example/demo/rest/controller/TopicsController.java
+++ back-end/src/main/java/com/example/demo/rest/controller/TopicsController.java
@@ -1,8 +1,9 @@
-package com.example.demo.controller;
+package com.example.demo.rest.controller;
 
-import com.example.demo.dto.TopicsDto;
-import com.example.demo.mapper.TopicsMapper;
+
 import com.example.demo.model.Topics;
+import com.example.demo.rest.dto.TopicsDto;
+import com.example.demo.rest.mapper.TopicsMapper;
 import com.example.demo.service.TopicsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -17,21 +18,21 @@ public class TopicsController {
     TopicsMapper topicsMapper;
     @GetMapping("/topics")
     public List<TopicsDto> getAllTopics(){
-        return topicsMapper.mapToDto(topicsService.getAllTopics());
+        return topicsMapper.toDto(topicsService.getAllTopics());
     }
     @GetMapping("/id")
     public TopicsDto getAllTopicsById(@RequestParam long id){
-        return topicsMapper.mapToDto(topicsService.getAllTopicsById(id));
+        return topicsMapper.toDto(topicsService.getAllTopicsById(id));
     }
     @PostMapping("/topics")
     public String addTopics(@RequestBody TopicsDto topicsDto){
-        Topics topics=topicsMapper.mapToEntity(topicsDto);
+        Topics topics=topicsMapper.toEntity(topicsDto);
         topicsService.addTopics(topics);
         return "Added Successfully";
     }
     @PutMapping("/update")
     public String updateTopics(@RequestBody TopicsDto topicsDto){
-        Topics topics=topicsMapper.mapToEntity(topicsDto);
+        Topics topics=topicsMapper.toEntity(topicsDto);
         topicsService.updateTopics(topics);
         return "Updated Successfully";
     }
---
+++ back-end/src/main/java/com/example/demo/rest/dto/TopicsDto.java
@@ -0,0 +1,10 @@
+package com.example.demo.rest.dto;
+
+import lombok.Data;
+
+@Data
+public class TopicsDto {
+    private Long id;
+    private String name;
+    private String description;
+}
