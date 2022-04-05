d2094a9650f361c717fd443ccb79b98476de6fe8
Noorhn-Khaled <100763814+Noorhan-Khaled@users.noreply.github.com>
2022-04-05 10:16:15 +0200

    solve error

--- back-end/src/main/java/com/example/demo/dto/TopicLevelDto.java
+++ back-end/src/main/java/com/example/demo/dto/TopicLevelDto.java
@@ -3,25 +3,24 @@ package com.example.demo.dto;
 import javax.persistence.Column;
 
 public class TopicLevelDto {
-    private Long topicLevelId;
-    private String topicLevelDescribtion;
+    private Long id;
+    private String describtion;
     private PositionDto position;
-    private Long positionId;
 
-    public Long getTopicLevelId() {
-        return topicLevelId;
+    public Long getId() {
+        return id;
     }
 
-    public void setTopicLevelId(Long topicLevelId) {
-        this.topicLevelId = topicLevelId;
+    public void setId(Long id) {
+        this.id = id;
     }
 
-    public String getTopicLevelDescribtion() {
-        return topicLevelDescribtion;
+    public String getDescribtion() {
+        return describtion;
     }
 
-    public void setTopicLevelDescribtion(String topicLevelDescribtion) {
-        this.topicLevelDescribtion = topicLevelDescribtion;
+    public void setDescribtion(String describtion) {
+        this.describtion = describtion;
     }
 
     public PositionDto getPosition() {
@@ -39,4 +38,8 @@ public class TopicLevelDto {
     public void setPositionId(Long positionId) {
         this.positionId = positionId;
     }
+
+    private Long positionId;
+
+
 }
