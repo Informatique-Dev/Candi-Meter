55f1e65128606f54ac8f4f46ff9a352eb3828c0c
Noorhn-Khaled <100763814+Noorhan-Khaled@users.noreply.github.com>
2022-04-05 10:16:15 +0200

    solve error

--- back-end/src/main/java/com/example/demo/rest/controller/PositionController.java
+++ back-end/src/main/java/com/example/demo/rest/controller/PositionController.java
@@ -1,8 +1,8 @@
-package com.example.demo.rest.controller;
+package com.example.demo.controller;
 
+import com.example.demo.dto.PositionDto;
+import com.example.demo.mapper.PositionMapper;
 import com.example.demo.model.Position;
-import com.example.demo.rest.dto.PositionDto;
-import com.example.demo.rest.mapper.PositionMapper;
 import com.example.demo.service.PositionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -14,27 +14,25 @@ public class PositionController {
     @Autowired
     PositionService positionService;
     PositionMapper positionMapper;
-
     @GetMapping("/position")
     public List<PositionDto> getAllPosition(){
 
-        return positionMapper.toDto(positionService.getAllPosition());
+        return positionMapper.mapToDto(positionService.getAllPosition());
     }
     @GetMapping("/id")
     public PositionDto getPositionById(@RequestParam long id){
 
-        return positionMapper.toDto(positionService.getPositionById(id));
+        return positionMapper.mapToDto(positionService.getPositionById(id));
     }
     @PostMapping("/position")
     public String addPosition(@RequestBody PositionDto positionDto){
-        Position position=positionMapper.toEntity(positionDto);
-
+        Position position=positionMapper.mapToEntity(positionDto);
         positionService.addPosition(position);
         return "Saved successfully";
     }
     @PutMapping("/update")
     public String updatePosition(@RequestBody PositionDto positionDto){
-        Position position=positionMapper.toEntity(positionDto);
+        Position position=positionMapper.mapToEntity(positionDto);
         positionService.updatePosition(position);
         return "Updated successfully";
     }
--- back-end/src/main/java/com/example/demo/rest/controller/TopicLevelController.java
+++ back-end/src/main/java/com/example/demo/rest/controller/TopicLevelController.java
@@ -1,13 +1,15 @@
-package com.example.demo.rest.controller;
+package com.example.demo.controller;
 
+import com.example.demo.dto.TopicLevelDto;
+import com.example.demo.mapper.TopicLevelMapper;
+import com.example.demo.model.Position;
 import com.example.demo.model.TopicLevel;
-import com.example.demo.rest.dto.TopicLevelDto;
-import com.example.demo.rest.mapper.TopicLevelMapper;
 import com.example.demo.service.TopicLevelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/topiclevel")
@@ -18,22 +20,22 @@ public class TopicLevelController {
     @GetMapping("/topiclevel")
     public List<TopicLevelDto> getAllTopicLevel(){
 
-        return topicLevelMapper.toDto(topicLevelService.getAllTopicLevel());
+        return topicLevelMapper.mapToDto(topicLevelService.getAllTopicLevel());
     }
     @GetMapping("/id")
     public TopicLevelDto getTopicLevelById(@RequestParam long id){
 
-        return topicLevelMapper.toDto(topicLevelService.getTopicLevelById(id));
+        return (topicLevelMapper.mapToDto((topicLevelService.getTopicLevelById(id))));
     }
     @PostMapping("/topiclevel")
     public String addTopicLevel(@RequestBody TopicLevelDto topicLevelDto){
-        TopicLevel topicLevel=topicLevelMapper.toEntity(topicLevelDto);
+        TopicLevel topicLevel=topicLevelMapper.mapToEntity(topicLevelDto);
         topicLevelService.addTopicLevel(topicLevel);
         return "Saved Successfully";
     }
     @PutMapping("/topiclevel")
     public String updateTopicLevel(@RequestBody TopicLevelDto topicLevelDto){
-        TopicLevel topicLevel=topicLevelMapper.toEntity(topicLevelDto);
+        TopicLevel topicLevel=topicLevelMapper.mapToEntity(topicLevelDto);
         topicLevelService.updateTopicLevel(topicLevel);
         return "Updated Successfully";
     }
