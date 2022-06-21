58383f2f1df4e2b81d98f876f71daf1e3ba81b52
Noorhn-Khaled <100763814+Noorhan-Khaled@users.noreply.github.com>
2022-06-21 14:14:48 +0200

    topic-level

--- back-end/src/main/java/com/example/demo/rest/controller/TopicLevelController.java
+++ back-end/src/main/java/com/example/demo/rest/controller/TopicLevelController.java
@@ -15,39 +15,39 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("/topiclevel")
+@RequestMapping("/topic-level")
 @AllArgsConstructor
-@Tag(name = "topiclevel", description = "REST API for topiclevel")
+@Tag(name = "topic-level", description = "REST API for topic-level")
 public class TopicLevelController {
     private TopicLevelHandler topicLevelHandler;
 
     @GetMapping
-    @Operation(summary = "get All topiclevel ")
+    @Operation(summary = "get All topic-level ")
     public ResponseEntity getAll(@RequestParam(value = "page", defaultValue = "0") Integer page,
                                  @RequestParam(value = "size", defaultValue = "10") Integer size) {
         return topicLevelHandler.getAll(page, size);
     }
 
     @GetMapping("/{id}")
-    @Operation(summary = "Get topiclevel By Id")
+    @Operation(summary = "Get topic-level By Id")
     public ResponseEntity<?> getById(@PathVariable(value = "id") Integer id) {
         return topicLevelHandler.getById(id);
     }
 
     @PostMapping
-    @Operation(summary = "Add New topiclevel")
+    @Operation(summary = "Add New topic-level")
     public ResponseEntity save(@RequestBody TopicLevelDto dto) {
         return topicLevelHandler.save(dto);
     }
 
     @PutMapping("/{id}")
-    @Operation(summary = "Update A topiclevel")
+    @Operation(summary = "Update A topic-level")
     public ResponseEntity update(@PathVariable Integer id, @RequestBody TopicLevelDto dto) {
         return topicLevelHandler.update(id, dto);
     }
 
     @DeleteMapping("/{id}")
-    @Operation(summary = "Delete A topiclevel")
+    @Operation(summary = "Delete A topic-level")
     public ResponseEntity delete(@PathVariable Integer id) {
         return topicLevelHandler.delete(id);
     }
--- back-end/src/main/java/com/example/demo/rest/dto/PositionDto.java
+++ back-end/src/main/java/com/example/demo/rest/dto/PositionDto.java
@@ -10,6 +10,4 @@ public class PositionDto extends RestDto {
 
     private String name;
     private String describtion;
-
-
 }
