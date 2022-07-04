4f31f30efea9e792172e3a29d5b9d0cd7da2a4a1
Tasneem-Gad <tgad@sirius-its.com>
2022-07-04 16:14:04 +0200

    createAuthGuardand change home copmponent place to pages folder

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,17 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ContactUsComponent } from './Pages/contact-us/contact-us.component';
-import { HomePageComponent } from './core/components/home-page/home-page.component';
 import { MainComponent } from './core/components/main/main.component';
 import { PageNotFoundComponent } from './Pages/page-not-found/page-not-found.component';
 
 const routes: Routes = [
 
     { path:"main", component:MainComponent, children:[
-    {path:"home", component: HomePageComponent},
     { path: '',loadChildren: () => import('./Pages/pages.module').then(m => m.PagesModule)},
-
-
   ] ,
 },
 { path: '',loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)},
