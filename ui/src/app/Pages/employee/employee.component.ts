ec2bec0453440a4e8da889fa521448146b4ea0c7
Tasneem-Gad <tgad@sirius-its.com>
2022-07-07 15:30:02 +0200

    CreateEmployeePage

---
+++ ui/src/app/Pages/employee/employee.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.sass']
+})
+export class EmployeeComponent implements OnInit {
+  employeeForm!:FormGroup;
+  displayedColumns: string[] = ['Question', 'description'];
+  isVisible:boolean = false;
+  constructor(private formBuilder:FormBuilder) { }
+  ngOnInit(): void {
+    this.employForm();
+  }
+  employForm(){
+    this.employeeForm=this.formBuilder.group({
+      id:[''],
+      name:['', [Validators.required]],
+      company_id:['', [Validators.required]]
+    })
+  }
+  onSubmit(){
+
+  }
+  changeVisibility(){
+    this.isVisible = !this.isVisible
+  }
+}
