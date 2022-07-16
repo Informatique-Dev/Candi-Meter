539ae8a6bce76ae15f51ec1696bdfd05704ac016
AshrakatAbdELSattar <aabdelsattar@informatique-eg.com>
2022-07-17 00:05:10 +0200

    #70 added common columns in restDto

--- back-end/src/main/java/com/example/demo/rest/dto/common/RestDto.java
+++ back-end/src/main/java/com/example/demo/rest/dto/common/RestDto.java
@@ -4,10 +4,17 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
+import java.time.LocalDateTime;
+
 @Data
 @EqualsAndHashCode(of = "id", callSuper = false)
 @ToString(of = "id", callSuper = false)
 public class RestDto {
 
     private Integer id;
+    private Integer version;
+    private String createdBy;
+    private String updatedBy;
+    private LocalDateTime createdDate;
+    private LocalDateTime updateDate;
 }
