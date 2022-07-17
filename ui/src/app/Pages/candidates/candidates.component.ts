d31aec0d5f46215a149c94484d1a4f4c2b930955
Tasneem-Gad <tgad@sirius-its.com>
2022-07-17 14:59:09 +0200

    solve_Conflict

---
+++ ui/src/app/Pages/candidates/candidates.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-candidates',
+  templateUrl: './candidates.component.html',
+  styleUrls: ['./candidates.component.sass']
+})
+export class CandidatesComponent implements OnInit {
+
+  constructor(private fb: FormBuilder) { }
+  candidateForm : FormGroup;
+  ngOnInit(): void {
+    this.candidateForm = this.fb.group({
+    'name':['',Validators.required],
+    'phone':['',Validators.required],
+    'email':['',Validators.required],
+    'cv':['',Validators.required]
+    })
+  }
+  onSubmit(){
+
+  }
+
+}
--- ui/src/app/Pages/pages.module.ts
+++ ui/src/app/Pages/pages.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { SharedModule } from 'src/shared/shared-module/shared.module';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { RouterModule, Routes } from '@angular/router';
 import { QuestionComponent } from './AddCandidateInfo/question/question.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CompanyComponent } from './company/company.component';
 import { EmployeeComponent } from './employee/employee.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 const routes: Routes = [
@@ -30,8 +30,10 @@ const routes: Routes = [
   imports: [
     RouterModule.forChild(routes),
     CommonModule,
+    ReactiveFormsModule,
     SharedModule,
-    ReactiveFormsModule
-    ]
+    RouterModule.forChild(routes)
+
+  ]
 })
 export class PagesModule { }
