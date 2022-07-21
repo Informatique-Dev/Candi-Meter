88e18369043c5636fe03623de338b545d25bfc5c
Tasneem-Gad <tgad@sirius-its.com>
2022-07-21 14:02:49 +0200

    change in candidate  respository

--- ui/src/app/domain/candidate/candidate.repository.ts
+++ ui/src/app/domain/candidate/candidate.repository.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { SettingsService } from 'src/app/core/services/config/settings.service';
 import { Candidate } from "./models/candidate";
 import { ResourceService } from 'src/app/core/services/config/resource.service';
-import { HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -17,4 +17,23 @@ export class CandidateRepository extends ResourceService<Candidate> {
   getResourceUrl(): string {
     return 'candidate';
   };
+
+  toServerModel(entity: Candidate): any {
+    if (!entity.id) {
+      return {
+        name: entity.name,
+        phone: entity.phone,
+        email: entity.email
+      };
+    } else {
+      return {
+        id: entity.id,
+        name: entity.name,
+        phone: entity.phone,
+        email: entity.email,
+        documentId: entity.documentId,
+      };
+    }
+  };
+
 }
