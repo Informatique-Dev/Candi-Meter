c8ce8969c18b9637503668514d8903baff8da11c
Tasneem-Gad <tgad@sirius-its.com>
2022-07-24 17:02:28 +0200

    get all companies and show company in tables

--- ui/src/app/Pages/company/company.component.ts
+++ ui/src/app/Pages/company/company.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PageEvent } from '@angular/material/paginator';
+import { Company } from 'src/app/domain/company/models/company';
 import { CompanyRepository } from '../../domain/company/company.repository';
 
 @Component({
@@ -9,12 +11,17 @@ import { CompanyRepository } from '../../domain/company/company.repository';
 })
 export class CompanyComponent implements OnInit {
   companyForm!:FormGroup;
-  displayedColumns: string[] = ['id', 'name','description'];
+  displayedColumns: string[] = ['id', 'name','description','update','delete'];
   isVisible:boolean = false;
+  allCompanies:Company[]=[];
+  size:number=0;
+  page:number=0;
+  totalRows:number=0;
   constructor(private formBuilder:FormBuilder,private companyRepository:CompanyRepository) { }
 
   ngOnInit(): void {
     this.compForm();
+    this.getAllCompanies();
   }
   compForm(){
     this.companyForm=this.formBuilder.group({
@@ -30,10 +37,18 @@ export class CompanyComponent implements OnInit {
     }
   }
   addCompany(){
-    this.companyRepository.add(this.companyForm.value).subscribe(res=>{
-     console.log('add');
+    this.companyRepository.add(this.companyForm.value).subscribe(_=>{ })
+  }
+  getAllCompanies(){
+    this.companyRepository.getList().subscribe(result=>{
+      this.allCompanies = result.data;
     })
 
+  }
+  pageChange(event:PageEvent){
+     this.size= event.pageSize;
+     this.page= event.pageIndex;
+     this.getAllCompanies();
   }
   changeVisibility(){
     this.isVisible = !this.isVisible
