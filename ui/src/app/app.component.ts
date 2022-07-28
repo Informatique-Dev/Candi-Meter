f86a613b59279dd52811cf334442018d7649ecdc
Tasneem-Gad <tgad@sirius-its.com>
2022-07-28 14:53:36 +0200

    change in app component

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,17 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { HotToastService } from '@ngneat/hot-toast';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent implements OnInit {
+export class AppComponent{
   title = 'candi-meter';
-  constructor(private toast: HotToastService){
-
-  }
-  ngOnInit(): void {
-    this.toast.success('Hello World!');
-  }
 
 }
