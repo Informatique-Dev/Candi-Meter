017b232b384a8b742a35893bf68a73226d78af7e
AshrakatAbdELSattar <aabdelsattar@informatique-eg.com>
2022-07-28 16:16:16 +0200

    #83 add api to upload & download document

--- back-end/src/main/java/com/example/demo/rest/handler/DocumentHandler.java
+++ back-end/src/main/java/com/example/demo/rest/handler/DocumentHandler.java
@@ -9,6 +9,13 @@ import com.example.demo.support.DocumentType;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URI;
+import java.util.Optional;
 
 @Component
 @AllArgsConstructor
@@ -17,13 +24,52 @@ public class DocumentHandler {
     private DocumentService documentService;
     private DocumentMapper documentMapper;
 
-    public DocumentDto uploadDocument(DocumentDto documentDto, DocumentType documentType) {
+   /* public DocumentDto uploadDocument(DocumentDto documentDto, DocumentType documentType) {
         Document document = documentMapper.toEntity(documentDto);
         document.setDocumentType(documentType);
         Document save = documentService.save(document);
         return documentMapper.toDto(save);
-    }
+    }*/
 
+   public ResponseEntity uploadDocument(MultipartFile file)  {
+       Document document = new Document();
+       document.setDocumentName(file.getOriginalFilename());
+       document.setDocumentType(DocumentType.CANDIDATE_CV);
+       document.setContentType(file.getContentType());
+       document.setSize(file.getSize());
+
+       try {
+           document.setContent(file.getBytes());
+      } catch (IOException e) {
+           throw new RuntimeException(e);
+       }
+
+       documentService.save(document);
+       return ResponseEntity.created(URI.create("/document/" + document.getId()))
+               .body(documentMapper.toDto(document));
+
+   }
+    public ResponseEntity<?> downloadDocument(Integer id, HttpServletResponse response){
+        Optional<Document> result= documentService.getById(id);
+        Document document = result.get();
+        response.setContentType("application/octet-stream");
+
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=" + document.getDocumentName();
+
+        response.setHeader(headerKey,headerValue);
+
+        ServletOutputStream outputStream = null;
+        try {
+            outputStream = response.getOutputStream();
+            outputStream.write(document.getContent());
+            outputStream.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return ResponseEntity.ok(documentMapper.toDto(document));
+    }
     public ResponseEntity<?> getById(Integer id) {
         Document document = documentService.getById(id)
                 .orElseThrow(() -> new ResourceNotFoundException(Document.class.getSimpleName(), id));
