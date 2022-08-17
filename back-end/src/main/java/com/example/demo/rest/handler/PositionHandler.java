1f081a4eb74c79deca2d4e234117a6b375ed7d7f
AshrakatAbdELSattar <aabdelsattar@informatique-eg.com>
2022-08-17 16:27:54 +0200

    #107 validate position name update naming

--- back-end/src/main/java/com/example/demo/rest/handler/PositionHandler.java
+++ back-end/src/main/java/com/example/demo/rest/handler/PositionHandler.java
@@ -40,8 +40,8 @@ public class PositionHandler {
     }
 
     public ResponseEntity<?> save(PositionDto dto) {
-        Optional<Position> existingPositionName = positionService.getByName(dto.getName());
-        if (existingPositionName.isPresent()) {
+        Optional<Position> existingName = positionService.getByName(dto.getName());
+        if (existingName.isPresent()) {
             return ResponseEntity.status(HttpStatus.CONFLICT).build();
         }
         Position position = positionMapper.toEntity(dto);
@@ -53,8 +53,8 @@ public class PositionHandler {
     public ResponseEntity<?> update(Integer id, PositionDto dto){
         Position position = positionService.getById(id).
                 orElseThrow(() -> new ResourceNotFoundException(Position.class.getSimpleName(), id));
-        Optional<Position> existingPositionName = positionService.getByName(dto.getName());
-        if (existingPositionName.isPresent() && existingPositionName.get().getId().equals(id)) {
+        Optional<Position> existingName = positionService.getByName(dto.getName());
+        if (existingName.isPresent() && existingName.get().getId().equals(id)) {
             return ResponseEntity.status(HttpStatus.CONFLICT).build();
         }
         positionMapper.updateEntityFromDto(dto, position);
