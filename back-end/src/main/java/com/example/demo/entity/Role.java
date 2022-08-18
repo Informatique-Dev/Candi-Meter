917b305f1448e3c94952cf718c89e72992784338
hossamgomaa211@gmail.com <hossamgomaa211@gmail.com>
2022-08-18 14:44:07 +0200

    #112 add security

---
+++ back-end/src/main/java/com/example/demo/entity/Role.java
@@ -0,0 +1,25 @@
+package com.example.demo.entity;
+
+import com.example.demo.entity.common.JPAEntity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@Entity
+@EqualsAndHashCode(of = "id", callSuper = false)
+@ToString(of = "id", callSuper = false)
+@Table(name = "role")
+public class Role extends JPAEntity {
+    @Column(name = "name")
+    private String name;
+
+    @ManyToMany
+    @JoinTable(name = "user_role",
+            joinColumns =@JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id"))
+    private List<User> users;
+}
